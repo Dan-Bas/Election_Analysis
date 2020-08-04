62398f844312001c79a9cf6cb5b97979c2ee0f00
Dan-Bas <67299365+Dan-Bas@users.noreply.github.com>
2020-08-04 00:33:23 -0400

    Python practice

---
+++ Python_practice.py
@@ -0,0 +1,93 @@
+counties = ['Arapahoe','Denver','Jefferson']
+if counties[1] == 'Denver':
+    print(counties[1])
+
+temperature = int(input("What is the temperature outside? "))
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")    
+
+#What is the score?
+score = int(input('What is your test score?'))
+
+#Determine the grade.
+if score >= 90:
+    print('Your grade is an A')
+else:
+    if score >= 80:
+        print('Your grade is a B')
+    else:
+        if score >= 70:
+            print('Your grade is a C')
+        else:
+            if score >= 60:
+                print('Your grade is a D')
+            else:
+                print('Your grade is an F')
+
+#What is the score?
+score = int(input('What is your test score?'))
+
+# Determine the grade
+if score >= 90:
+    print('Your grade is an A')
+elif score >= 80:
+    print('Ypur grade is a B')
+elif score >= 70:
+    print('Your grade is a C')
+elif score >= 60:
+    print('Your grade is a D')
+else:
+    print('Your score is an F')        
+
+x = 0
+while x <= 5:
+    print (x)
+    x = x + 1
+
+for county in counties:
+    print(county)
+
+numbers = [0,1,2,3,4]
+for num in numbers:
+    print(num)
+
+for num in range(9):
+    print(num)
+
+
+for i in range(len(counties)):    
+    print(counties[i])
+
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county in counties_dict:
+    print(county)
+
+for county in counties_dict.keys():
+    print(county)
+
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+
+for county, voters in counties_dict.items():
+    print(county,voters) 
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+for county_dict in voting_data:
+    print(county_dict['county'])        

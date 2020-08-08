0da52a8d9986634ef0fbc4d0ece1ea71fa041693
Dan-Bas <67299365+Dan-Bas@users.noreply.github.com>
2020-08-07 23:14:24 -0400

    files

--- Python_practice.py
+++ Python_practice.py
@@ -91,3 +91,60 @@ for county_dict in voting_data:
 
 for county_dict in voting_data:
     print(county_dict['county'])        
+
+print ("Arapahoe and Denver are not in the list of counties")
+
+interest = str
+print("Your interest for the year is $" + str(2))
+
+
+my_votes = int(input("How many votes did you get in the election?"))
+total_votes = int(input("What is the total votes in the election?"))
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes")
+
+
+my_votes = int(input("How many votes did you get in the election?"))
+total_votes = int(input("What is the total votes in the election?"))
+print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jeferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+
+candidate_votes = int(input("How many votes did the candidate get in the election?"))
+total_votes = int(input("What is the total votes in the election?"))
+message_to_candidate = (f"You received {candidate_votes:,} number of votes."
+                        f"The total number of votes in the election was {total_votes:,} "
+                        f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes")
+
+print(message_to_candidate)                        
+
+
+# Import the datetime class from the datetime modole
+import datetime
+# Use the now() attribute on the datetime class to get the present time
+now = datetime.datetime.now()
+# Print the present time
+print("The time right now is ", now)
+
+
+dir({'Arapahoe': 422829, 'Denver': 463353, 'Jefferson': 432438})
+
+dir(int)
+dir(float)
+
+dir(csv)
+
+
+import csv
+dir(csv)
+
+dir(csv)
+
